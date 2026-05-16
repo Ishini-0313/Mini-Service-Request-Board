db82fa5744a47000d7f563814a6db5100f466d99
Ishini-0313 <misnfernando1@gmail.com>
2026-05-16 21:44:31 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+require("dotenv").config();
+const jobRoutes = require("./routes/jobRoutes");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.json());
+app.use(jobRoutes); 
 
 app.listen(5000, () => {
     console.log("Server running on port 5000");
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  allowedDevOrigins: ['192.168.192.1'],
 };
 
 export default nextConfig;
