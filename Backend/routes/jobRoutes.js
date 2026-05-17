32cff2c8470b0ce8943558c16e04f670cc4be4c0
Ishini-0313 <misnfernando1@gmail.com>
2026-05-17 17:06:58 +0530

    protect function added

--- Backend/routes/jobRoutes.js
+++ Backend/routes/jobRoutes.js
@@ -2,16 +2,18 @@ const express = require('express');
 
 const {getAllJobs, getJobById, createJob, updateJobStatus, deleteJob} = require('../controllers/jobController.js');
 
+const protect = require('../middleware/authMiddleware.js');
+
 const router = express.Router();
 
 router.get('/api/jobs', getAllJobs);
 
 router.get('/api/jobs/:id', getJobById);
 
-router.post('/api/jobs', createJob);
+router.post('/api/jobs', protect, createJob);
 
 router.put('/api/jobs/:id', updateJobStatus);
 
-router.delete('/api/jobs/:id', deleteJob);
+router.delete('/api/jobs/:id', protect, deleteJob);
 
 module.exports = router;
